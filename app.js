b89ca0961987761fb69fcff661cad13c717b3e96
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-08-28 15:33:07 +0200

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
+console.log("charles")
 
 console.log(validator.isEmail("andrew@example.com"))
 console.log(validator.isURL("www.panenco.com"))
