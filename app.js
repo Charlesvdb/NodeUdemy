63e90ee6cf2424112d399992739a3ae826229dcf
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-08-31 16:41:30 +0200

    adding a note

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
-const validator = require("validator")
-const getNotes = require('./notes.js')
-const chalk = require("chalk")
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
-console.log("charles")
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(validator.isEmail("andrew@example.com"))
-console.log(validator.isURL("www.panenco.com"))
-console.log(chalk.green("Success!"))
+// add, remove, read and list
+// 1. Create add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function (argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
 
+// 2. Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function (){
+        console.log("Removing the note!")
+    }
+})
+
+// 3. Create list command
+yargs.command({
+    command: "list",
+    describe: "list your notes",
+    handler: function (){
+        console.log("Listing out all notes!")
+    }
+})
+
+// 4. Create read command
+yargs.command({
+    command: "read",
+    describe: "read a note",
+    handler: function (){
+        console.log("Reading a note!")
+    }
+})
+
+console.log(process.argv)
+console.log(yargs.argv)
